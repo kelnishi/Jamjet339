1216aa5fe3dd1e7fb3063af469cb9b7c951946a0
Kelvin Nishikawa <kelvin.nishikawa@gmail.com>
2013-09-28 14:27:11 -0700

    Initial commit

---
+++ Assets/FlightController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FlightController : MonoBehaviour {
+	
+	public Vector3 lift = Vector3.up;
+	public Vector3 thrust = Vector3.zero;
+	public Rigidbody liftBody;
+	public Rigidbody thrustBody;
+	
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (liftBody) {
+			liftBody.AddForce(lift/(rigidbody.mass*liftBody.mass));	
+		}
+		if (thrustBody) {
+			thrustBody.AddRelativeForce(thrust);
+		}
+	}
+}
